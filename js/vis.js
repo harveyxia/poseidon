0f15d4dcf8600a9fa33f7cb884950b60ec451bdb
Harvey Xia <harvey@harveyxia.com>
2014-06-12 13:57:04 -0400

    Start drawing lines

--- js/vis.js
+++ js/vis.js
@@ -24,19 +24,47 @@ function draw_vis() {
 
   var x = d3.time.scale()
     .domain([new Date(data[0].time), new Date(data[data.length - 1].time)])
-    .range([0, width]);
+    .range([50, width - 100]);
 
-console.warn([new Date(data[0].time), new Date(data[data.length - 1].time)]);
+  var x_axis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .ticks(d3.time.minutes, 10)
+    .tickFormat(d3.time.format('%X'))
+    .tickSize(2);
 
-  var point = vis.selectAll('g')
+    vis.append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', 'translate(0, ' + (height - 200) + ')')
+      .call(x_axis);
+
+  var y = d3.scale.linear()
+    .domain([0, 1])
+    .range([height, 0]);
+    var y_axis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+
+  var yt_line_function = d3.svg.line()
+    .x(function(d) { return x(new Date(d.time))} )
+    .y(function(d) { return y('www.youtube.com' in d.tabs) })
+    .interpolate('linear');
+    var yt_line = vis.append('path')
+      .attr('d', yt_line_function(data))
+      .attr('stroke', 'blue')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none');
+
+  var point = vis.selectAll('circle')
     .data(data)
-    .enter().append('g')
-    .attr()
-
-  point.append('circle')
+    .enter().append('circle')
     .attr('cx', function(d) { return x(new Date(d.time))})
     .attr('cy', 5)
     .attr('r', 2.5);
+
+  // point.append('circle')
+    
   
 }
 
@@ -46,8 +74,14 @@ function format_json(json) {
   for (var item in json) {
     var obj = {
       time: item,
-      tabs: json[item]
+      tabs: {}
+      // tabs: json[item]
     };
+    json[item].forEach(function(tab) {
+      // var tab_obj = {};
+      // tab_obj[tab.url] = { active: tab.active };
+      obj.tabs[tab.url] = { active: tab.active };
+    });
     new_json.push(obj);
   }
   // sort the array by date to ensure order
